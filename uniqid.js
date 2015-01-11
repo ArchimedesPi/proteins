2a038cdc4a5613088afc324766123aa9d9364cd0
Liam Marshall <archimedespi3141@gmail.com>
2015-01-10 21:23:30 -0600

    Using array of keys for protein structure hashing

--- uniqid.js
+++ uniqid.js
@@ -1,7 +1,14 @@
 var crypto = require('crypto');
+var _ = require('underscore');
+var debug = require('debug')('uniqid');
 
 module.exports = function (protein) {
 	var hash = crypto.createHash('md5');
-	hash.update(protein.fullName.toLowerCase());
+	var proteinHashes = ['fullName']
+	_.each(proteinHashes, function (proteinkey) {
+		hash.update(protein[proteinkey].toLowerCase().replace(/\s+/g, ''));
+		debug("hashed " + proteinkey);
+	});
+	debug("done updating hashes");
 	return hash.digest('hex');
 };
