ad6805980c9301d16a39a0d48c0a4ff0e66dc2de
Liam Marshall <archimedespi3141@gmail.com>
2015-01-10 21:26:35 -0600

    Added new key to id hash

--- uniqid.js
+++ uniqid.js
@@ -4,7 +4,7 @@ var debug = require('debug')('uniqid');
 
 module.exports = function (protein) {
 	var hash = crypto.createHash('md5');
-	var proteinHashes = ['fullName']
+	var proteinHashes = ['fullName', 'speciesOfOrigin']
 	_.each(proteinHashes, function (proteinkey) {
 		hash.update(protein[proteinkey].toLowerCase().replace(/\s+/g, ''));
 		debug("hashed " + proteinkey);
