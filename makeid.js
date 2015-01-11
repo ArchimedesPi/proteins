83c0bae823a691aecae577a6b5b39c125d8dfc9c
Liam Marshall <archimedespi3141@gmail.com>
2015-01-10 21:23:11 -0600

    enable debug in script

--- makeid.js
+++ makeid.js
@@ -2,6 +2,7 @@
 
 var crypto = require('crypto');
 var Protein = require('./protein');
+var debug = require('debug')('makeid')
 
 require('cli').withStdin(function (stdin) {
 	protein = new Protein();
