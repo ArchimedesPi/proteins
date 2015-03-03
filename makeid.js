641d79cece674d872a15185a1e399a9e88599f92
Liam M <ArchimedesPi@users.noreply.github.com>
2015-03-03 17:56:33 -0600

    Fix syntax issues

--- makeid.js
+++ makeid.js
@@ -2,10 +2,10 @@
 
 var crypto = require('crypto');
 var Protein = require('./protein');
-var debug = require('debug')('makeid')
+var debug = require('debug')('makeid');
 
 require('cli').withStdin(function (stdin) {
-	protein = new Protein();
+	var protein = new Protein();
 	protein.load(stdin);
 
 	console.log(protein.id());
