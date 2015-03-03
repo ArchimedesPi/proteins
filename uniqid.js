49c94052ce0b0c1365b219b3073230368afdf57c
Liam M <ArchimedesPi@users.noreply.github.com>
2015-03-03 17:59:29 -0600

    Yay another missing semicolon

--- uniqid.js
+++ uniqid.js
@@ -4,7 +4,7 @@ var debug = require('debug')('uniqid');
 
 module.exports = function (protein) {
 	var hash = crypto.createHash('md5');
-	var proteinHashes = ['fullName', 'organism']
+	var proteinHashes = ['fullName', 'organism'];
 	_.each(proteinHashes, function (proteinkey) {
 		debug("hashing " + proteinkey);
 		hash.update(protein[proteinkey].toLowerCase().replace(/\s+/g, ''));
